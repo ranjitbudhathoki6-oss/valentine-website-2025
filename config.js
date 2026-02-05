1eb6252d98f8afcf6a28954055f185edd2dd55dd
ranjitbudhathoki6-oss <ranjitbudhathoki6@gmail.com>
2026-02-05 13:19:06 +1100

    Update config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        message: "Now come get your gift, a big warm hug and a huge kiss! I love you to the moon and back",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
